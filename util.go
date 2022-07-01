b6050340fdd2d7713feda37c0c062f3d4901c580
zerodoc <danielc@smallwoodhome.com>
2022-07-01 09:59:40 -0500

    add context on onexit function

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package zdgoutil
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -54,6 +55,22 @@ func OnExit(fn func(os.Signal, ...interface{}), args ...interface{}) {
 	signal.Notify(sigChan, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGILL, syscall.SIGTERM)
 
 	sig := <-sigChan
+	fmt.Println(" - cleanup request started with", sig, "signal")
+	fn(sig, args...)
+}
+
+// OnExitWithContext calls function before exiting program and if context cancel, will stop listening for on exit events
+func OnExitWithContext(ctx context.Context, fn func(os.Signal, ...interface{}), args ...interface{}) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGILL, syscall.SIGTERM)
+
+	var sig os.Signal
+	select {
+	case <-ctx.Done():
+		return
+	case sig = <-sigChan:
+	}
+
 	fmt.Println(" - cleanup request started with", sig, "signal")
 	fn(sig, args...)
 }
